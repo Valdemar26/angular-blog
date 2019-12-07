99596d484d739b4db6271806d56f4bd1910d679c
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2019-12-07 19:27:51 +0200

    8: handle errors

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 import { FirebaseAuthResponse, UserInterface } from '../interfaces';
 import { environment } from '../../../../environments/environment';
@@ -9,6 +9,8 @@ import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>();
 
   constructor(private http: HttpClient) {}
 
@@ -31,7 +33,8 @@ export class AuthService {
       user
     )
       .pipe(
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       );
   }
 
@@ -41,6 +44,24 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return !!this.token;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error;
+
+    switch (message) {
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Даний Email не зареєстровано');
+        break;
+      case 'INVALID_EMAIL':
+        this.error$.next('Неправильний Email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неправильний Password');
+        break;
+    }
+
+    return throwError(error);
   }
 
   private setToken(response: FirebaseAuthResponse | null) {
