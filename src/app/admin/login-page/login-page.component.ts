0001af7e12ef15cbe6d0db0a5d4e990c5d3591f2
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2019-12-07 18:38:54 +0200

    7: save firebase token in localStorage, login and logout logic

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -13,6 +13,7 @@ import { AuthService } from '../shared/services/auth.service';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
 
   constructor(private auth: AuthService, private router: Router) { }
 
@@ -28,6 +29,8 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submitted = true;
+
     const user: UserInterface = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -36,7 +39,8 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe( () => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
-    })
+      this.submitted = false;
+    });
 
   }
 
