662b74a9525a877a9bb538be058b73441db7f6e2
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-12-11 12:35:02 +0200

    18: post page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { PostsService } from '../shared/posts.service';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  posts$: Observable<Post[]>;
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
+    this.posts$ = this.postsService.getAllPosts();
   }
 
 }
