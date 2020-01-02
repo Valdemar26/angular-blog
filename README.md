6f67f89a5d00b7efd4ec981d71f0c0ab7d4cbcd0
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2020-01-02 21:02:04 +0200

    create scroll() mixin, add time$ stream for clock

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 See demo at https://angular-blog-ua.firebaseapp.com/
 
 ## Development server
+Run `nvm use 10.9` to choose NodeJs version 10.9
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -12,10 +12,21 @@ import { Post } from '../shared/interfaces';
 export class HomePageComponent implements OnInit {
 
   posts$: Observable<Post[]>;
+  public time$: Observable<Date>;
+
   constructor(private postsService: PostsService) { }
 
   ngOnInit() {
     this.posts$ = this.postsService.getAllPosts();
+    this.getCurrentTime();
+  }
+
+  getCurrentTime() {
+    this.time$ = new Observable( obs => {
+      setInterval(() => {
+        obs.next(new Date());
+      }, 1000);
+    });
   }
 
 }
