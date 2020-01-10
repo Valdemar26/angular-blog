672d9c7b70a7bf3cfd18ec968f77fa6f20bab868
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2020-01-10 17:27:03 +0200

    23: search with rxjs

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,9 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Observable, of } from 'rxjs';
 
 import { PostsService } from '../shared/posts.service';
 import { Post } from '../shared/interfaces';
-import {debounceTime, filter, map, tap} from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-page',
@@ -27,7 +27,7 @@ export class HomePageComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     // this.onSearchChange().subscribe();
-    console.log(this.searchInput.nativeElement);
+    // console.log(this.searchInput.nativeElement);
   }
 
   getCurrentTime() {
@@ -38,12 +38,29 @@ export class HomePageComponent implements OnInit, AfterViewInit {
     });
   }
 
-  // onSearchChange(value) {
+  onSearchChange(value) {
+    console.log(value);
   // behavior subject
   // this.someBeh.next(value)
   //   return fromEvent( this.searchInput.nativeElement, "input" ).pipe(
   //     tap((value) => console.log(value))
   //   );
-  // }
+
+    fromEvent(document.querySelector('#search'), 'keyup')
+      .pipe(
+        map(e => (e.target as HTMLTextAreaElement).value),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap(val =>
+          of(val).pipe(
+            tap(v => {
+              console.log(v);
+            }
+            )
+          )
+        )
+      ).subscribe(data => console.log('DATA: ', data));
+
+  }
 
 }
