69305549564870cf629bb927bb5c1fb27e0a9a04
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2020-06-10 14:40:45 +0300

    28: added vertical scroll indicator on post page

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,15 @@ import { Router } from '@angular/router';
 export class MainLayoutComponent implements OnInit {
 
   constructor(private router: Router) { }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    // TODO rewrite on Renderer2 and Angular way
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById('progressBar').style.width = scrolled + '%';
+  }
 
   ngOnInit() {
   }
